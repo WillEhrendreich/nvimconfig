a2bc777dc1c9b5e0ccd072e6e363a6c21a8a7513
WillEhrendreich <will.ehrendreich@gmail.com>
2022-07-02 17:16:42 -0500

    set up for omnisharp and ionide

--- lsp.lua
+++ lsp.lua
@@ -1,49 +1,122 @@
+local getDotNetRoot = function(filepath)
+  local lsp = require 'lspconfig'
+  local root = lsp.util.find_git_ancestor(filepath)
+  return root
+end
+
 return {
   servers = {
     "clangd",
     "cmake",
-    "cssls",
+    -- "cssls",
     "html",
-    "intelephense",
+    -- "intelephense",
     "jsonls",
     "pyright",
-    "sqls",
+    -- "sqls",
+    "ionide",
+    "omnisharp",
     "sumneko_lua",
-    "texlab",
+    -- "texlab",
     "tsserver",
     "yamlls",
   },
-  skip_setup = { "tsserver", "clangd" },
+  plugins = {
+    ["nvim-lsp-installer"] = {
+      ensure_installed = {
+        "omnisharp",
+        "fsautocomplete",
+      },
+    },
+  },
+
+  skip_setup = { "tsserver", "clangd", "fsautocomplete" },
   ["server-settings"] = {
-    clangd = { capabilities = { offsetEncoding = "utf-8" } },
+    clangd = { capabilities = { offsetencoding = "utf-8" } },
     pyright = {
       settings = {
         python = {
           analysis = {
-            typeCheckingMode = "on",
+            typecheckingmode = "on",
           },
         },
       },
     },
-    sqls = {
-      on_attach = function(client, bufnr)
-        if client.name == "sqls" then
-          require("sqls").on_attach(client, bufnr)
-        end
+
+    omnisharp = {
+      root_dir = getDotNetRoot,
+      log_level = 2,
+      settings =
+      {
+        FileOptions =
+        {
+          ExcludeSearchPatterns = {
+            '**/node_modules/**/*',
+            '**/bin/**/*',
+            '**/obj/**/*',
+            '/tmp/**/*'
+          }
+          ,
+          SystemExcludeSearchPatterns = {
+            '**/node_modules/**/*',
+            '**/bin/**/*',
+            '**/obj/**/*',
+            '/tmp/**/*'
+          }
+          ,
+        },
+        FormattingOptions = { EnableEditorConfigSupport = true },
+        ImplementTypeOptions =
+        {
+          InsertionBehavior = 'WithOtherMembersOfTheSameKind',
+          PropertyGenerationBehavior = 'PreferAutoProperties',
+        },
+        RenameOptions =
+        {
+          RenameInComments = true,
+          RenameInStrings  = true,
+          RenameOverloads  = true,
+        },
+        RoslynExtensionsOptions =
+        {
+          EnableAnalyzersSupport = true,
+          EnableDecompilationSupport = true,
+          LocationPaths =
+          {
+            -- 		"~/.omnisharp/Roslynator/src/Analyzers.CodeFixes/bin/Debug/netstandard2.0",
+            -- 		"~/.omnisharp/Roslynator/src/Analyzers/bin/Debug/netstandard2.0",
+            -- 		"~/.omnisharp/Roslynator/src/CodeAnalysis.Analyzers.CodeFixes/bin/Debug/netstandard2.0",
+            -- 		"~/.omnisharp/Roslynator/src/CodeAnalysis.Analyzers/bin/Debug/netstandard2.0",
+            -- 		"~/.omnisharp/Roslynator/src/CodeFixes/bin/Debug/netstandard2.0",
+            -- 		"~/.omnisharp/Roslynator/src/CommandLine/bin/Debug/netstandard2.0",
+            -- 		"~/.omnisharp/Roslynator/src/Common/bin/Debug/netstandard2.0",
+            -- 		"~/.omnisharp/Roslynator/src/Core/bin/Debug/netstandard2.0",
+            -- 		"~/.omnisharp/Roslynator/src/CSharp.Workspaces/bin/Debug/netstandard2.0",
+            -- 		"~/.omnisharp/Roslynator/src/CSharp/bin/Debug/netstandard2.0",
+            -- 		"~/.omnisharp/Roslynator/src/Documentation/bin/Debug/netstandard2.0",
+            -- 		"~/.omnisharp/Roslynator/src/Formatting.Analyzers.CodeFixes/bin/Debug/netstandard2.0",
+            -- 		"~/.omnisharp/Roslynator/src/Formatting.Analyzers/bin/Debug/netstandard2.0",
+            -- 		"~/.omnisharp/Roslynator/src/Refactorings/bin/Debug/netstandard2.0",
+            -- 		"~/.omnisharp/Roslynator/src/Workspaces.Common/bin/Debug/netstandard2.0",
+            -- 		"~/.omnisharp/Roslynator/src/Workspaces.Core/bin/Debug/netstandard2.0",
+          },
+        },
+      },
+    },
+    ionide = {
+      root_dir = getDotNetRoot,
+    },
+    sumneko_lua = {
+      on_attach = function(client)
+        client.server_capabilities.document_formatting = false
+        print("sumneko lua attaching to " ..
+          vim.fn.expand("%") ..
+          " and document formatting is set to " .. vim.inspect(client.server_capabilities.document_formatting))
       end,
     },
-    texlab = {
-      settings = {
-        texlab = {
-          build = { onSave = true },
-          forwardSearch = {
-            executable = "zathura",
-            args = { "--synctex-forward", "%l:1:%f", "%p" },
-          },
-        },
-      },
-    },
     yamlls = {
+
+
       settings = {
         yaml = {
           schemas = {
@@ -54,5 +127,25 @@ return {
         },
       },
     },
+    -- sqls = {
+    --   on_attach = function(client, bufnr)
+    --     if client.name == "sqls" then
+    --       require("sqls").on_attach(client, bufnr)
+    --     end
+    --   end,
+    -- },
+
+    -- texlab = {
+    --   settings = {
+    --     texlab = {
+    --       build = { onsave = true },
+    --       forwardsearch = {
+    --         executable = "zathura",
+    --         args = { "--synctex-forward", "%l:1:%f", "%p" },
+    --       },
+    --     },
+    --   },
+    -- },
+    --
   },
 }
