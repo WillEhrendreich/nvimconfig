4f3ec1ac60dc5667bbacd8e8f06c35ca23448299
WillEhrendreich <will.ehrendreich@gmail.com>
2022-07-02 17:14:43 -0500

    added shell autoread tabstops etc

--- options.lua
+++ options.lua
@@ -1,5 +1,9 @@
 return {
   opt = {
+    relativenumber = true,
+    autoread = true,
+    tabstop = 2,
+    shiftwidth = 2,
     conceallevel = 2, -- enable conceal
     foldenable = false,
     foldexpr = "nvim_treesitter#foldexpr()", -- set Treesitter based folding
@@ -12,5 +16,11 @@ return {
     spellfile = "~/.config/nvim/lua/user/spell/en.utf-8.add",
     thesaurus = "~/.config/nvim/lua/user/spell/mthesaur.txt",
     wrap = true, -- soft wrap lines
+    shell ="pwsh",
+    shellcmdflag = '-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;',
+    shellredir = '2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode',
+    shellpipe = '2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode',
+    shellquote = "",
+    shellxquote = "",
   },
 }
