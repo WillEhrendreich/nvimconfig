ed4913eaa8004902fc4a7a6261c92d400ee5ba61
WillEhrendreich <will.ehrendreich@gmail.com>
2022-07-05 10:51:06 -0500

    changed to new layout

--- plugins/dapui.lua
+++ plugins/dapui.lua
@@ -1,32 +1,68 @@
-return {
+local dap_status_ok, dap = pcall(require, "dap")
+if not dap_status_ok then
+	return
+end
+
+local dap_ui_status_ok, dapui = pcall(require, "dapui")
+if not dap_ui_status_ok then
+	return
+end
+
+dapui.setup {
   icons = { expanded = "▾", collapsed = "▸" },
   mappings = {
-    expand = "<cr>",
+    -- Use a table to apply multiple mappings
+    expand = { "<CR>", "<2-LeftMouse>" },
     open = "o",
     remove = "d",
     edit = "e",
     repl = "r",
     toggle = "t",
   },
-  sidebar = {
-    elements = {
-      { id = "scopes", size = 0.5 },
-      { id = "breakpoints", size = 0.25 },
-      { id = "stacks", size = 0.25 },
+  layouts = {
+    sidebar = {
+      -- You can change the order of elements in the sidebar
+      elements = {
+        -- Provide as ID strings or tables with "id" and "size" keys
+        {
+          id = "scopes",
+          size = 0.25, -- Can be float or integer > 1
+        },
+        { id = "breakpoints", size = 0.25 },
+        -- { id = "stacks", size = 0.25 },
+        -- { id = "watches", size = 00.25 },
+      },
+      size = 40,
+      position = "right", -- Can be "left", "right", "top", "bottom"
     },
-    size = 40,
-    position = "right",
-  },
-  tray = {
-    elements = { "repl" },
-    size = 10,
-    position = "bottom",
+    tray = {
+      elements = {},
+      -- elements = { "repl" },
+      -- size = 10,
+      -- position = "bottom", -- Can be "left", "right", "top", "bottom"
+    },
   },
   floating = {
-    border = "rounded",
+    max_height = nil, -- These can be integers or a float between 0 and 1.
+    max_width = nil, -- Floats will be treated as percentage of your screen.
+    border = "rounded", -- Border style. Can be "single", "double" or "rounded"
     mappings = {
-      close = { "q", "<esc>" },
+      close = { "q", "<Esc>" },
     },
   },
   windows = { indent = 1 },
 }
+
+local icons = require "user.icons"
+
+vim.fn.sign_define('DapBreakpoint', {text=icons.ui.Bug, texthl='DiagnosticSignError', linehl='', numhl=''})
+
+dap.listeners.after.event_initialized["dapui_config"] = function()
+  dapui.open()
+end
+dap.listeners.before.event_terminated["dapui_config"] = function()
+  dapui.close()
+end
+dap.listeners.before.event_exited["dapui_config"] = function()
+  dapui.close()
+end
