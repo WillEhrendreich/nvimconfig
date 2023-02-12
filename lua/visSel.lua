82f81bfc1da58da545000275dc542c5d680b6368
WillEhrendreich <will.ehrendreich@gmail.com>
2023-02-12 17:26:24 -0600

    lots of changes, lsp,cmp,neoconf,etc.

---
+++ lua/visSel.lua
@@ -0,0 +1,93 @@
+local M = {}
+
+function M.GetVisualSelection(keepSelectionIfNotInBlockMode, advanceCursorOneLine, debugNotify)
+  local line_start, column_start
+  local line_end, column_end
+  -- if debugNotify is true, use vim.notify to show debug info.
+  debugNotify = debugNotify or false
+  -- keep selection defaults to false, but if true the selection will
+  -- be reinstated after it's cleared to set '> and '<
+  -- only relevant in visual or visual line mode, block always keeps selection.
+  keepSelectionIfNotInBlockMode = keepSelectionIfNotInBlockMode or false
+  -- advance cursor one line defaults to true, but is turned off for
+  -- visual block mode regardless.
+  advanceCursorOneLine = (function()
+    if keepSelectionIfNotInBlockMode == true then
+      return false
+    else
+      return advanceCursorOneLine or true
+    end
+  end)()
+
+  if vim.fn.visualmode() == "\22" then
+    line_start, column_start = unpack(vim.fn.getpos("v"), 2)
+    line_end, column_end = unpack(vim.fn.getpos("."), 2)
+  else
+    -- if not in visual block mode then i want to escape to normal mode.
+    -- if this isn't done here, then the '< and '> do not get set,
+    -- and the selection will only be whatever was LAST selected.
+    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<esc>", true, false, true), 'x', true)
+    line_start, column_start = unpack(vim.fn.getpos("'<"), 2)
+    line_end, column_end = unpack(vim.fn.getpos("'>"), 2)
+  end
+  if (column_start) > (column_end) then
+    column_start, column_end = column_end, column_start
+    if debugNotify == true then
+      vim.notify("switching column start and end, \nWas " ..
+        column_end .. "," .. column_start .. "\nNow " .. column_start .. "," .. column_end)
+    end
+  end
+  if (line_start) > (line_end) then
+    line_start, line_end = line_end, line_start
+    if debugNotify == true then
+      vim.notify("switching line start and end, \nWas " ..
+        line_end .. "," .. line_start .. "\nNow " .. line_start .. "," .. line_end)
+    end
+  end
+  if vim.g.selection == "exclusive" then
+    column_end = column_end - 1 -- Needed to remove the last character to make it match the visual selection
+  end
+  if debugNotify == true then
+    vim.notify("vim.fn.visualmode(): " ..
+      vim.fn.visualmode() ..
+      "\nsel start " ..
+      vim.inspect(line_start) ..
+      " " .. vim.inspect(column_start) .. "\nSel end " .. vim.inspect(line_end) .. " " .. vim.inspect(column_end))
+  end
+  local n_lines = math.abs(line_end - line_start) + 1
+  local lines = vim.api.nvim_buf_get_lines(0, line_start - 1, line_end, false)
+  if #lines == 0 then
+    return { "" }
+  end
+  if vim.fn.visualmode() == "\22" then
+    -- this is what actually sets the lines to only what is found between start and end columns
+    for i = 1, #lines do
+      lines[i] = string.sub(lines[i], column_start, column_end)
+    end
+  else
+    lines[1] = string.sub(lines[1], column_start, -1)
+    if n_lines == 1 then
+      lines[n_lines] = string.sub(lines[n_lines], 1, column_end - column_start + 1)
+    else
+      lines[n_lines] = string.sub(lines[n_lines], 1, column_end)
+    end
+    -- if advanceCursorOneLine == true, then i do want the cursor to advance once.
+    if advanceCursorOneLine == true then
+      if debugNotify == true then
+        vim.notify("advancing cursor one line past the end of the selection to line " .. vim.inspect(line_end + 1))
+      end
+      vim.api.nvim_win_set_cursor(0, { line_end + 1, 0 })
+    end
+
+    if keepSelectionIfNotInBlockMode then
+      vim.api.nvim_feedkeys("gv", "n", true)
+    end
+  end
+  if debugNotify == true then
+    vim.notify(table.concat(lines, "\n"))
+  end
+  return lines -- use this return if you want an array of text lines
+  -- return table.concat(lines, "\n") -- use this return instead if you need a text block
+end
+
+return M
