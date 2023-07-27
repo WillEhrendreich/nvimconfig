edd89e7aeabc2606b49dbe28bcc1231ffc49ce1b
WillEhrendreich <will.ehrendreich@gmail.com>
2023-07-27 11:13:27 -0500

    Update csharpls.lua: Modify csharp language server setup
    - the setup function was in the wrong scope.
    
    These changes update the configuration of the csharp language server.

--- lua/plugins/csharpls.lua
+++ lua/plugins/csharpls.lua
@@ -9,11 +9,11 @@ return {
       -- you can do any additional lsp server setup here
       -- return true if you don't want this server to be setup with lspconfig
       ---@type table<string, fun(server:string, opts:_.lspconfig.options):boolean?>
-      setup = {
-        ---@type  fun(server:string, opts:_.lspconfig.options)
-        csharp_ls = function(server, opts) end,
-        -- all seperate lsp servers have thier own setup files, for clarity
-      },
+    },
+    setup = {
+      ---@type  fun(server:string, opts:_.lspconfig.options)
+      csharp_ls = function(server, opts) end,
+      -- all seperate lsp servers have thier own setup files, for clarity
     },
   },
 }
