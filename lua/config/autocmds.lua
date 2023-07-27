a83cc41869401978d33ecec61f705cad3df2e272
WillEhrendreich <will.ehrendreich@gmail.com>
2023-07-27 11:06:37 -0500

    Refactor autocmds.lua: Update json filetype and add moon filetype support
    - Added autocmd for `BufReadPost` to set the filetype to `jsonc` for JSON files.
    - Added autocmd for `FileType` to set the syntax to `moon` for MoonScript files.
    - Added autocmd for `BufReadPost` and `FileType` to automatically re-read the Lua file when the MoonScript file changes.
    
    These changes improve the handling of JSON and MoonScript files in the autocmds configuration.

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -18,6 +18,58 @@ autocmd({ "BufNewFile", "BufReadPre", "FileType" }, {
   end,
 })
 
+autocmd("BufReadPost", {
+  desc = "Make sure the filetype automatically is set to include comments.",
+  group = grp("MakeJsonFTJsonC", { clear = true }),
+  pattern = { "json", "*.json" },
+  callback = function(_)
+    vim.notify("im totally going to set this filetype to jsonc. dawg")
+
+    vim.schedule_wrap(function()
+      vim.notify("im totally going to set this filetype to jsonc. dawg")
+      vim.cmd("set filetype=jsonc")
+      vim.cmd("set syntax=jsonc")
+      vim.bo.filetype("jsonc")
+      vim.notify(
+        "im totally going to set this filetype to " .. vim.opt.filetype .. " which by this point i hope is jsonc. dawg"
+      )
+    end)
+  end,
+})
+
+autocmd("FileType", {
+  desc = "Make sure the moon type automatically compiles on save ",
+  group = grp("MoonAutoCompile", { clear = true }),
+  pattern = "*.moon",
+  callback = function(ev)
+    vim.cmd("set syntax=moon")
+
+    print(string.format("event fired: %s", vim.inspect(ev)))
+  end,
+})
+
+autocmd({ "BufReadPost", "FileType" }, {
+  desc = "Make sure the lua transpiled from moon type automatically re-reads on moon change",
+  pattern = "*.moon",
+  group = grp("MoonDerivedAutoRead", { clear = true }),
+  callback = function(ev)
+    vim.schedule(function()
+      local thisActualBufnum = vim.api.nvim_get_current_buf()
+      local luaBuffer = (vim.fn.filter(vim.api.nvim_list_bufs(), function(x)
+        local basename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(thisActualBufnum), ":p:r")
+        local moonfileName = basename .. ".moon"
+        local otherName = basename .. ".lua"
+        return otherName == vim.fn.fnamemodify(vim.api.nvim_buf_get_name(x), ":p")
+      end))[1]
+      if luaBuffer and vim.api.nvim_buf_is_loaded(luaBuffer) then
+        vim.cmd.buffer(luaBuffer)
+        vim.cmd("e!")
+        vim.cmd.buffer(thisActualBufnum)
+      end
+    end)
+  end,
+})
+
 grp("CppCommands", { clear = true })
 autocmd({ "BufNewFile", "BufReadPre", "FileType" }, {
   desc = "changes comment string for cpp and similar files ",
