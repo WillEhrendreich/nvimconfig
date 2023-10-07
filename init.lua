f1f7c4baff27ffa61cbabdcb674a69d8d5099a53
Will Ehrendreich <Will.Ehrendreich@CFR.Priv>
2023-10-06 20:52:37 -0500

    feat: added automatic install via chocolatey. it's awesome.

--- init.lua
+++ init.lua
@@ -1,6 +1,469 @@
+-- local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+-- if not vim.loop.fs_stat(lazypath) then
+--   -- bootstrap lazy.nvim
+--   -- stylua: ignore
+--   vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
+-- end
+-- vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
+-- local lazyCore = vim.fn.stdpath("data") .. "/lazy/lazy.nvim/lua/lazy/core/"
+-- vim.opt.rtp:prepend(lazyCore)
+-- local lazyConfig = require("lazy.core.config").setup()
+-- local lazyview = vim.fn.stdpath("data") .. "/lazy/lazy.nvim/lua/lazy/view/"
+-- vim.opt.rtp:prepend(lazyview)
+local function fileExists(filePath)
+  local file = io.open(filePath, "r")
+  if file then
+    file:close()
+    return true
+  end
+  return false
+end
+
+local function getFileLastModified(filePath)
+  local fileInfo = vim.loop.fs_stat(filePath)
+  if fileInfo then
+    return fileInfo.mtime.sec
+  end
+  return nil
+end
+
+local function isFileOlderThanOneDay(filePath)
+  local lastModified = getFileLastModified(filePath)
+  -- vim.notify(filePath .. "LastModified: " .. vim.inspect(lastModified))
+  if lastModified then
+    local currentTime = os.time()
+    local oneDayInSeconds = 24 * 60 * 60
+    return lastModified < (currentTime - oneDayInSeconds)
+  end
+  return false
+end
+
+local function runCommandIfFileOlderThanOneDay(filePath, command)
+  if (not fileExists(filePath)) or isFileOlderThanOneDay(filePath) then
+    -- vim.notify("executing {" .. command .. "}")
+    vim.fn.system(command)
+  else
+    -- vim.notify("file {" .. filePath .. "} was last modified in the last day, keeping it. ")
+  end
+end
+
+local function extractPackageId(inputString)
+  local pattern = '<package id="([^"]-)" />'
+  local packageName = inputString:match(pattern)
+  return packageName
+end
+
+function UseChocoToInstallAllTheThings()
+  -- local currentPackagesExportPath = "c:\\temp\\packages.config"
+  local currentPackagesExportPath = "c:/temp/packages.config"
+  local currentPackagesConfig = vim.fs.find("packages.config", { path = "c:\\temp\\" })[1] or ""
+  if not currentPackagesConfig == currentPackagesExportPath then
+    -- vim.notify("running choco Export to " .. currentPackagesExportPath)
+    vim.fn.system("choco export " .. currentPackagesExportPath)
+  else
+    -- vim.notify("running choco export if the file is missing or older than a day " .. currentPackagesExportPath)
+    runCommandIfFileOlderThanOneDay(currentPackagesExportPath, "choco export " .. currentPackagesExportPath)
+  end
+
+  local chocoPackagesConfig = vim.fs.find("packages.config", { path = vim.fn.stdpath("config") })[1] or ""
+  -- vim.notify(
+  --   "found packages.config in config dir that had these packages:\n" .. vim.inspect(installsFromPackagesConfig)
+  -- )
+  local allPackagesLinesFromDistro = {}
+  for i, p in ipairs(vim.fn.readfile(chocoPackagesConfig, "")) do
+    table.insert(allPackagesLinesFromDistro, p)
+  end
+
+  local allPackagesLinesFromCurrent = {}
+  for i, p in ipairs(vim.fn.readfile(currentPackagesExportPath, "")) do
+    table.insert(allPackagesLinesFromCurrent, p)
+  end
+
+  -- local installsFromPackagesConfig = {}
+  local differences = {}
+  for i, p in ipairs(allPackagesLinesFromDistro) do
+    if not vim.tbl_contains(allPackagesLinesFromCurrent, p) then
+      local chocoPackage = extractPackageId(p)
+      table.insert(differences, chocoPackage)
+    end
+  end
+
+  local packageToExecutableName = {
+
+    ["7zip"] = "7z",
+    ["autohotkey"] = "autohotkey",
+    ["bottom"] = "btm",
+    ["chocolatey"] = "choco",
+    ["curl"] = "curl",
+    ["docker"] = "docker",
+    ["docker-cli"] = "docker",
+    ["dotnet"] = "dotnet",
+
+    -- ["dotnet-6.0-desktopruntime"] = "dotnet ",
+    -- ["dotnet-7.0-desktopruntime"] = "dotnet-7.0-desktopruntime",
+    -- ["dotnet-7.0-sdk-4xx"] = "dotnet-7.0-sdk-2xx",
+    -- ["dotnet-desktopruntime"] = "dotnet-desktopruntime",
+    ["drawio"] = "drawio",
+    ["fzf"] = "fzf",
+    ["gdu"] = "gdu",
+    ["gh"] = "gh",
+    ["gimp"] = "gimp",
+    ["git"] = "git",
+    -- ["github-desktop"] = "github-desktop",
+    ["groupy"] = "groupy",
+    ["gzip"] = "gzip",
+    ["ilspy"] = "ilspy",
+    -- ["jetbrainsmono"] = "jetbrainsmono",
+    ["lazygit"] = "lazygit",
+    ["llvm"] = "clang",
+    ["messenger"] = "messenger",
+    ["mingw"] = "gcc",
+    -- ["nerd-fonts-DelugiaMono-Powerline"] = "nerd-fonts-DelugiaMono-Powerline",
+    -- ["nerd-fonts-Hack"] = "nerd-fonts-Hack",
+    -- ["nerd-fonts-JetBrainsMono"] = "nerd-fonts-JetBrainsMono",
+    -- ["nerd-fonts-ProggyClean"] = "nerd-fonts-ProggyClean",
+    -- ["nerd-fonts-SpaceMono"] = "nerd-fonts-SpaceMono",
+    -- ["nerd-fonts-VictorMono"] = "nerd-fonts-VictorMono",
+    ["nodejs"] = "npm",
+    ["notepadplusplus"] = "notepad++",
+    ["oh-my-posh"] = "oh-my-posh",
+    ["opera-gx"] = "opera-gx",
+    ["paint.net"] = "paint.net",
+    ["powertoys"] = "powertoys",
+    ["pwsh"] = "pwsh",
+    ["python"] = "python",
+    ["rainmeter"] = "rainmeter",
+    ["ripgrep"] = "rg",
+    ["SQLite"] = "SQLite3",
+    ["steam"] = "steam",
+    -- ["sumatrapdf"] = "sumatra",
+    ["terminal-icons.powershell"] = "terminal-icons.powershell",
+    ["tree-sitter"] = "tree-sitter",
+    ["vcredist140"] = "vcredist140",
+    ["vlc"] = "vlc",
+    ["vscode"] = "code",
+    ["Wget"] = "Wget",
+    ["wiztree"] = "wiztree",
+    ["zig"] = "zig",
+    ["cutepdf"] = "cutepdf",
+  }
+
+  local installsThatGetCalledFromNeovim = {
+
+    "7zip",
+    "bottom",
+    "chocolatey",
+    "curl",
+    "docker",
+    "fzf",
+    "gdu",
+    "gh",
+    "git",
+    "gzip",
+    "lazygit",
+    "llvm",
+    "nodejs",
+    "pwsh",
+    "python",
+    "ripgrep",
+    "SQLite",
+    "tree-sitter",
+    "Wget",
+    "zig",
+  }
+  local AllInstalled = true
+  local notInstalled = {}
+  for i, pack in ipairs(installsThatGetCalledFromNeovim) do
+    local p = (packageToExecutableName[pack] or pack)
+    -- vim.notify("checking for program: " .. p)
+    local installed = vim.fn.executable(p) == 1
+    -- local installed = vim.fn.system("choco " .. p) == 1
+
+    if installed then
+      -- vim.notify("program: " .. p .. " is installed and executable. ")
+    else
+      -- vim.notify(
+      --   "program named: {"
+      --     .. p
+      --     .. "} is not executable. If it's intalled, then it's possible that it's parent directory is simply not included in the PATH variable. Adding it to the list of things chocolatey will try to install."
+      -- )
+      table.insert(notInstalled, pack)
+    end
+  end
+  if vim.tbl_count(notInstalled) > 0 then
+    -- vim.notify(
+    --   "Now going to call choco upgrade (which installs it if it's missing) on the following programs:\n"
+    --     .. table.concat(notInstalled, "\n")
+    -- )
+    -- vim.fn.system("choco upgrade " .. table.concat(installs, " ") .. " -y --whatif" )
+    -- local chocoCmd = "choco upgrade " .. table.concat(notInstalled, " ") .. " -y --whatif"
+    local chocoCmd = { "choco", "upgrade" }
+    vim.list_extend(chocoCmd, notInstalled)
+    -- vim.notify("going to run " .. chocoCmd)
+    -- vim.api.nvim
+    vim.api.nvim_create_autocmd("BufEnter", {
+      group = vim.api.nvim_create_augroup("ChocoInstall", { clear = true }),
+      -- filter = "",
+      once = true,
+      callback = function()
+        vim.notify("going to run " .. vim.inspect(chocoCmd))
+        local util = require("config.util")
+        util.float_term(chocoCmd)
+      end,
+    })
+
+    -- vim.fn.system(chocoCmd)
+    -- vim.fn.system("refreshenv")
+  else
+    -- vim.notify("all listed programs are installed and executable from neovim.")
+  end
+end
+
+function IsElevatedPowershell()
+  local result = false
+  result = string.match(
+    vim.fn.system(
+      "(New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)"
+    ),
+    "True"
+  ) == "True"
+  -- result = vim.g["IsRunningElevatedPowershell"]
+  -- vim.notify("ResultAfterCallback: " .. vim.inspect(result))
+
+  if result == true then
+    -- vim.notify("Running Powershell with elevated permissions.. ")
+  else
+    vim.notify("NOT Running Powershell with elevated permissions.. ")
+    vim.notify("Current shell variable: " .. vim.inspect(vim.o.shell))
+    -- vim.no
+  end
+
+  return result
+end
+-- UseChocoToInstallAllTheThings()
+
+-- vim.notify("elevated returned: " .. vim.inspect(isElevated()))
+function SetUpExternalExecutables()
+  local isWindows = vim.fn.has("win32") == 1
+  -- and vim.fn.executable("explorer") == 1 then
+  if isWindows then
+    -- vim.notify("Windows Detected")
+    local shell = vim.o.shell
+    local shellcmdflag = vim.o.shellcmdflag
+    local shellredir = vim.o.shellredir
+    local shellpipe = vim.o.shellpipe
+    local shellquote = vim.o.shellquote
+    local shellxquote = vim.o.shellxquote
+
+    local pwshCoreAvailable = vim.fn.executable("pwsh")
+
+    local isPowershell = vim.o.shell == "pwsh"
+      or vim.o.shell == "pwsh.exe"
+      or vim.o.shell == "powershell"
+      or vim.o.shell == "powershell.exe"
+    if not isPowershell then
+      if pwshCoreAvailable then
+        vim.o.shell = "pwsh"
+      else
+        vim.o.shell = "powershell"
+      end
+      vim.o.shellcmdflag =
+        "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;"
+      vim.o.shellredir = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode"
+      vim.o.shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode"
+      vim.o.shellquote = ""
+      vim.o.shellxquote = ""
+    end
+    isPowershell = vim.o.shell == "pwsh"
+      or vim.o.shell == "pwsh.exe"
+      or vim.o.shell == "powershell"
+      or vim.o.shell == "powershell.exe"
+    if isPowershell then
+      -- vim.notify("powershell set as shell .. vim.o.shell: " .. vim.o.shell)
+      local isElevated = IsElevatedPowershell()
+
+      local hasChoco = vim.fn.executable("choco") == 1
+      if hasChoco == true then
+        -- vim.notify("chocolatey is installed, you rock.")
+      else
+        if isElevated then
+          local choice = (
+            vim.fn.inputlist({
+              "No chocolatey package manager installed, would you like to download it? ",
+              "1. Absolutely, take me there!",
+              "2. No I don't like convenient things like package managers, I prefer to suffer.",
+            })
+          )
+          vim.notify(type(choice))
+          if choice == 1 then
+            -- function table.jobstart(cmd: any, opts?: table<string, any>)
+            --   -> number
+            --
+            --  Spawns {cmd} as a job.
+            --  If {cmd} is a List it runs directly (no 'shell').
+            --  If {cmd} is a String it runs in the 'shell', like this:
+            --
+            --    :call jobstart(split(&shell) + split(&shellcmdflag) + ['{cmd}'])
+            --
+            --  (See |shell-unquoting| for details.)
+            --
+            --  Example:
+            --
+            --    :call jobstart('nvim -h', {'on_stdout':{j,d,e->append(line('.'),d)}})
+            --
+            --  Returns |job-id| on success, 0 on invalid arguments (or job
+            --  table is full), -1 if {cmd}[0] or 'shell' is not executable.
+            --  The returned job-id is a valid |channel-id| representing the
+            --  job's stdio streams. Use |chansend()| (or |rpcnotify()| and
+            --  |rpcrequest()| if "rpc" was enabled) to send data to stdin and
+            --  |chanclose()| to close the streams without stopping the job.
+            --
+            --  See |job-control| and |RPC|.
+            --
+            --  NOTE: on Windows if {cmd} is a List:
+            --    - cmd[0] must be an executable (not a "built-in"). If it is
+            --      in $PATH it can be called by name, without an extension:
+            --
+            --        :call jobstart(['ping', 'neovim.io'])
+            --
+            --      If it is a full or partial path, extension is required: >
+            --        :call jobstart(['System32\ping.exe', 'neovim.io'])
+            --  <      - {cmd} is collapsed to a string of quoted args as expected
+            --      by CommandLineToArgvW https://msdn.microsoft.com/bb776391
+            --      unless cmd[0] is some form of "cmd.exe".
+            --
+            --  The job environment is initialized as follows:
+            --    $NVIM                is set to |v:servername| of the parent Nvim
+            --    $NVIM_LISTEN_ADDRESS is unset
+            --    $NVIM_LOG_FILE       is unset
+            --    $VIM                 is unset
+            --    $VIMRUNTIME          is unset
+            --  You can set these with the `env` option.
+            --
+            --  {opts} is a dictionary with these keys:
+            --    clear_env:  (boolean) `env` defines the job environment
+            --          exactly, instead of merging current environment.
+            --    cwd:        (string, default=|current-directory|) Working
+            --          directory of the job.
+            --    detach:     (boolean) Detach the job process: it will not be
+            --          killed when Nvim exits. If the process exits
+            --          before Nvim, `on_exit` will be invoked.
+            --    env:        (dict) Map of environment variable name:value
+            --          pairs extending (or replace with "clear_env")
+            --          the current environment. |jobstart-env|
+            --    height:     (number) Height of the `pty` terminal.
+            --    |on_exit|:    (function) Callback invoked when the job exits.
+            --    |on_stdout|:  (function) Callback invoked when the job emits
+            --          stdout data.
+            --    |on_stderr|:  (function) Callback invoked when the job emits
+            --          stderr data.
+            --    overlapped: (boolean) Set FILE_FLAG_OVERLAPPED for the
+            --          standard input/output passed to the child process.
+            --          Normally you do not need to set this.
+            --          (Only available on MS-Windows, On other
+            --          platforms, this option is silently ignored.)
+            --    pty:        (boolean) Connect the job to a new pseudo
+            --          terminal, and its streams to the master file
+            --          descriptor. `on_stdout` receives all output,
+            --          `on_stderr` is ignored. |terminal-start|
+            --    rpc:        (boolean) Use |msgpack-rpc| to communicate with
+            --          the job over stdio. Then `on_stdout` is ignored,
+            --          but `on_stderr` can still be used.
+            --    stderr_buffered: (boolean) Collect data until EOF (stream closed)
+            --          before invoking `on_stderr`. |channel-buffered|
+            --    stdout_buffered: (boolean) Collect data until EOF (stream
+            --          closed) before invoking `on_stdout`. |channel-buffered|
+            --    stdin:      (string) Either "pipe" (default) to connect the
+            --          job's stdin to a channel or "null" to disconnect
+            --          stdin.
+            --    width:      (number) Width of the `pty` terminal.
+            --
+            --  {opts} is passed as |self| dictionary to the callback; the
+            --  i
+            --  caller may set other keys to pass application-specific data.
+            --
+            --  Returns:
+            --    - |channel-id| on success
+            --    - 0 on invalid arguments
+            --    - -1 if {cmd}[0] is not executable.
+            --  See also |job-control|, |channel|, |msgpack-rpc|.
+            --
+            --
+            -- vim.fn.jobstart("[bool]([Security.Principal.WindowsPrincipal]  [Security.Principal.WindowsIdentity]::GetCurrent() ).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)",
+            --           {on_stdout=
+            --             function(i)
+            --               if i then
+            -- if type(i) == "boolean" then
+
+            -- if i == true then
+            vim.notify(
+              "You've chosen to download chocolatey, thereby winning at everything forever.. lets do that automatcally shall we? "
+            )
+            vim.fn.system(
+              "Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))"
+            )
+
+            local hasChoco2 = vim.fn.executable("choco") == 1
+            vim.notify(
+              "At this point choco should be installed! To really do this well, You're going to need to shut down the terminal, and restart this nvim distro. "
+            )
+            if hasChoco2 == true then
+              vim.notify(
+                "Nvim looked for choco as executable, and it looks great, continuing on to check all the needed installs for the system"
+              )
+            else
+              vim.notify(
+                "Nvim looked for choco as executable, couldn't find it, it looks like you really will have to restart your terminal before this continues so that environment variables can be reloaded."
+              )
+            end
+
+            --       else
+            --         vim.notify("Cant download Choco automatically, you're not in an elevated powershell. \nhere's the link:\n https://chocolatey.org/install#individual \nhop to it. ")
+            --       end
+            --         -- isAdmin = i
+            --       else
+            --         vim.notify("Output: ".. vim.inspect(i))
+            --       end
+            --     end
+            --   end
+            -- })
+            --   if isAdmin then
+            --
+            --
+            --
+            -- else
+            -- end
+            hasChoco = hasChoco2 == 1
+          else
+            vim.notify(
+              "well... I guess you don't like nice things... do you perhaps put your shoes on the wrong feet for discomfort too?"
+            )
+          end
+        end
+      end
+      if hasChoco == true then
+        UseChocoToInstallAllTheThings()
+      end
+    else
+      vim.notify(
+        "not running powershell as main shell, currently set to: "
+          .. vim.inspect(vim.o.shell)
+          .. "\nfrankly I'm not able to do this choco install thing without elevated powershell.. so.. fix it."
+      )
+    end
+    vim.o.shell = shell
+    vim.o.shellcmdflag = shellcmdflag
+    vim.o.shellredir = shellredir
+    vim.o.shellpipe = shellpipe
+    vim.o.shellquote = shellquote
+    vim.o.shellxquote = shellxquote
+  end
+end
+
 -- bootstrap lazy.nvim, LazyVim and your plugins
 if vim.g.vscode then
 else
+  SetUpExternalExecutables()
   if vim.g.fvim_loaded then
     vim.o.guifont = "Iosevka NF:h17"
     -- vim.o.guifont = "JetBrainsMono NF:h14"
@@ -108,13 +571,6 @@ else
     vim.g.neovide_cursor_animate_command_line = true
     --- vim redraw
     vim.cmd.redraw()
-  end
-  local utils = require("config.util")
-  local debugCodeium = utils.getEnvVariableOrDefault("DEBUG_CODEIUM", "info")
-  if debugCodeium and debugCodeium ~= "warn" and debugCodeium ~= "info" then
-    vim.opt.cmdheight = 1
-  else
-    vim.opt.cmdheight = 0
   end
 end
 require("config.lazy")
