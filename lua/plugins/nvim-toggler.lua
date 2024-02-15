5bbd8b7f910dcd89195fb3546b5512c0560047be
Will Ehrendreich <will.ehrendreich@gmail.com>
2024-02-15 14:57:33 -0600

    feat(toggler) new inversions added

--- lua/plugins/nvim-toggler.lua
+++ lua/plugins/nvim-toggler.lua
@@ -1,16 +1,23 @@
 return {
 
-        "nguyenvukhang/nvim-toggler",
-      config = function()
-        require("nvim-toggler").setup {
-          inverses = {
-                ["bad"] = "good",
-                ["up"] = "down",
-                ["left"] = "right",
-                ["1"] = "0",
-          },
-          remove_default_keybinds = true,
-        }
-      end,
-    
+  "nguyenvukhang/nvim-toggler",
+  config = function()
+    require("nvim-toggler").setup({
+      inverses = {
+        ["bad"] = "good",
+        ["up"] = "down",
+        ["left"] = "right",
+        ["1"] = "0",
+        [">"] = "<",
+        [">="] = "<=",
+        ["=="] = "!=",
+        ["++"] = "--",
+        ["+"] = "-",
+        ["yes"] = "no",
+        ["Some"] = "None",
+        ["Ok"] = "Error",
+      },
+      remove_default_keybinds = true,
+    })
+  end,
 }
