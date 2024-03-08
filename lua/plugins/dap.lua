e543ec4d504c0be9db5adfe7f1e251ff471ff6a0
Will Ehrendreich <will.ehrendreich@gmail.com>
2024-03-08 10:59:54 -0600

    chore():formatting

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -1235,10 +1235,22 @@ return {
     -- fancy UI for the debugger
     {
       "rcarriga/nvim-dap-ui",
-      -- stylua: ignore
       keys = {
-        { "<leader>du", function() require("dapui").toggle({}) end,  desc = "Dap UI" },
-        { "<leader>de", function() require("dapui").eval() end,      desc = "Eval",  mode = { "n", "v" } },
+        {
+          "<leader>du",
+          function()
+            require("dapui").toggle({})
+          end,
+          desc = "Dap UI",
+        },
+        {
+          "<leader>de",
+          function()
+            require("dapui").eval()
+          end,
+          desc = "Eval",
+          mode = { "n", "v" },
+        },
       },
       opts = {},
       config = function(_, opts)
