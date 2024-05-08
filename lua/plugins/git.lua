d99c4ce8c7c4b087f2e5cebdc13d7907d8089c92
Will Ehrendreich <will.ehrendreich@gmail.com>
2024-05-08 14:39:21 -0500

    feat(gh.nvim): added

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -15,4 +15,19 @@ return {
       },
     },
   },
+
+  {
+    "ldelossa/gh.nvim",
+    dependencies = {
+      {
+        "ldelossa/litee.nvim",
+        config = function()
+          require("litee.lib").setup()
+        end,
+      },
+    },
+    config = function()
+      require("litee.gh").setup()
+    end,
+  },
 }
