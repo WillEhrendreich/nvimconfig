5781b69a90eac68d577c0211d524c90cff4fd6c7
Will Ehrendreich <EHRWI@roehl.net>
2025-04-16 09:40:07 -0500

    re-enabled scissors for snippets

--- lua/plugins/scissors.lua
+++ lua/plugins/scissors.lua
@@ -1,7 +1,7 @@
 return {
-  -- "chrisgrieser/nvim-scissors",
+  "chrisgrieser/nvim-scissors",
   -- dependencies = { "nvim-telescope/telescope.nvim", "L3MON4D3/LuaSnip" },
-  -- opts = {
-  --   snippetDir = vim.fn.stdpath("config") .. "/snippets/",
-  -- },
+  opts = {
+    snippetDir = vim.fn.stdpath("config") .. "/snippets/",
+  },
 }
