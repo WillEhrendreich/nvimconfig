7ceda2453d41b57b0066f36f9753d2ca41ef57c1
Will Ehrendreich <WillEhrendreich@hagerman.com>
2025-07-29 08:19:26 -0500

    getting rid of old RTI auth. no need to switch anymore.

--- lua/plugins/gh-auth-switch.lua
+++ lua/plugins/gh-auth-switch.lua
@@ -6,10 +6,6 @@ local r = {
     dirs = {
       ["C:/Code/repos/Ionide-nvim/"] = "WillEhrendreich",
       ["C:/Code/repos/gh-auth-switch"] = "WillEhrendreich",
-      ["C:/Code/repos/source"] = "EHRWI_RTI",
-      ["C:/Code/repos/edi-api/"] = "EHRWI_RTI",
-      ["C:/Code/repos/edi-web-tools/"] = "EHRWI_RTI",
-      ["C:/Code/repos/customer/"] = "EHRWI_RTI",
     },
   },
   dependencies = {},
